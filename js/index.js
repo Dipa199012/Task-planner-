4157a11b061cf1392f0a926f57b95b22273f8621
Jimi Mendapara <jimmymendapara91@gmail.com>
2021-06-11 12:40:57 +1000

    final push for step6 with all error resolve

--- js/index.js
+++ js/index.js
@@ -13,63 +13,70 @@ const form = document.getElementById('collapseExample');
 const descId = document.getElementById("description_form");
 const descErrMsg = document.getElementById("desc_valid");
 
-const validateDesc=()=>{
-    if(nameId.value === "" || nameId.value.length<5){
-        errMsg.style.color='red';
-       return errMsg.innerHTML="<span>name can't be empty or smaller than 5</span>";
-       }
-     else if(descId.value === "" || descId.value.length>200){
-        descErrMsg.style.color='red';
-       return descErrMsg.innerHTML="<span>Description can't be empty or greater then 200 characters</span>";
-       }
-       else if(asignId.value === "" || asignId.value.length<5){
-        asignErrMsg.style.color='red';
-       return asignErrMsg.innerHTML="<span>assign value can't be empty or smaller than 5</span>";
-       }
-       else if(dateId.value === "" ){
-        dateErrMsg.style.color='red';
-       return dateErrMsg.innerHTML="<span>Invalid date</span>";
-       }
-       else if(statusId.value === ""){
-        statusErrMsg.style.color='red';
-       return statusErrMsg.innerHTML="<span>Status can't be empty</span>";
-       }
-       
-       else{
-           return  form.addEventListener('submit',dataCatching);
-       }
+
+function clearFormField() {
+    nameId.value = "";
+    descId.value = "";
+    asignId.value = "";
+    dateId.value = "";
+    statusId.value = "";
+    nameId.classList.remove("is-valid");
+    descId.classList.remove("is-valid");
+    asignId.classList.remove("is-valid");
+    dateId.classList.remove("is-valid");
+    statusId.classList.remove("is-valid");
 
 }
-addTaskBtn.addEventListener('click',validateDesc);
 
-const dataCatching=()=>{
-    errMsg.innerHTML="";
-    descErrMsg.innerHTML="";
-    asignErrMsg.innerHTML="";
-    dateErrMsg.innerHTML="";
-    statusErrMsg.innerHTML="";
-   
 
-    taskManager.addTask(
-        nameId.value,
-        descId.value,
-        asignId.value,
-        dateId.value,
-        statusId.value
-      );
-     console.log("Task Name :" +  nameId.value.length);
-     console.log("User Name:"+nameId.value);
-        console.log("Description:"+descId.value);
-        console.log("Assign To:"+asignId.value);
-        console.log("Date:"+dateId.value);
-        console.log("Status:"+statusId.value);
+const validateDesc = () => {
+    if (nameId.value === "" || nameId.value.length < 5) {
+        errMsg.style.color = 'red';
+        return errMsg.innerHTML = "<span>name can't be empty or smaller than 5</span>";
+    }
+    else if (descId.value === "" || descId.value.length > 200) {
+        descErrMsg.style.color = 'red';
+        return descErrMsg.innerHTML = "<span>Description can't be empty or greater then 200 characters</span>";
+    }
+    else if (asignId.value === "" || asignId.value.length < 5) {
+        asignErrMsg.style.color = 'red';
+        return asignErrMsg.innerHTML = "<span>assign value can't be empty or smaller than 5</span>";
+    }
+    else if (dateId.value === "") {
+        dateErrMsg.style.color = 'red';
+        return dateErrMsg.innerHTML = "<span>Invalid date</span>";
+    }
+    else if (statusId.value === "") {
+        statusErrMsg.style.color = 'red';
+        return statusErrMsg.innerHTML = "<span>Status can't be empty</span>";
+    }
 
-        nameId.value="";
-        descId.value="";
-        asignId.value="";
-        dateId.value="";
-        statusId.value="";
-      
+    else {
 
-       
+        errMsg.innerHTML = "";
+        descErrMsg.innerHTML = "";
+        asignErrMsg.innerHTML = "";
+        dateErrMsg.innerHTML = "";
+        statusErrMsg.innerHTML = "";
+        taskManager.addTask(
+            nameId.value,
+            descId.value,
+            asignId.value,
+            dateId.value,
+            statusId.value
+        );
+        console.log("Task Name :" + nameId.value.length);
+        console.log("User Name:" + nameId.value);
+        console.log("Description:" + descId.value);
+        console.log("Assign To:" + asignId.value);
+        console.log("Date:" + dateId.value);
+        clearFormField();
+
+    }
+
 }
+
+form.addEventListener('submit', validateDesc);
+
+
+
