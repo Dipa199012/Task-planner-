17ea26d9b7aac83f0c78a945f73f15477a8a683b
Jimi Mendapara <jimmymendapara91@gmail.com>
2021-06-11 11:14:02 +1000

    Data is success fully inserted in to array task 6

--- js/index.js
+++ js/index.js
@@ -1,84 +1,75 @@
+const taskManager = new TaskManager(0);
+
 const nameId = document.getElementById("name_form");
 const errMsg = document.getElementById("name_valid");
 const addTaskBtn = document.getElementById("submit_btn");
-
-const validateBox=()=>{
-    if(nameId.value === "" || nameId.value.length<5){
-        errMsg.style.color='red';
-       return errMsg.innerHTML="<span>name can't be empty or smaller than 5</span>";
-       }
-       else{
-        return errMsg.innerHTML="";
-       }
-};
-addTaskBtn.addEventListener('click',validateBox);
-
-//_____________ ____________ _____________ _________
-
+const dateId = document.getElementById("date_form");
+const dateErrMsg = document.getElementById("date_valid");
+const statusId = document.getElementById("inputStatus");
+const statusErrMsg = document.getElementById("status_valid");
+const asignId = document.getElementById("assigned_form");
+const asignErrMsg = document.getElementById("asign_valid");
+const form = document.getElementById('collapseExample');
 const descId = document.getElementById("description_form");
 const descErrMsg = document.getElementById("desc_valid");
 
 const validateDesc=()=>{
-    if(descId.value === "" || descId.value.length>200){
+    if(nameId.value === "" || nameId.value.length<5){
+        errMsg.style.color='red';
+       return errMsg.innerHTML="<span>name can't be empty or smaller than 5</span>";
+       }
+     else if(descId.value === "" || descId.value.length>200){
         descErrMsg.style.color='red';
        return descErrMsg.innerHTML="<span>Description can't be empty or greater then 200 characters</span>";
        }
-       else{
-        return descErrMsg.innerHTML="";
-       }
-};
-addTaskBtn.addEventListener('click',validateDesc);
-//_____________ ____________ _____________ _________
-
-const asignId = document.getElementById("assigned_form");
-const asignErrMsg = document.getElementById("asign_valid");
-
-const validateAsign=()=>{
-    if(asignId.value === "" || asignId.value.length<5){
+       else if(asignId.value === "" || asignId.value.length<5){
         asignErrMsg.style.color='red';
        return asignErrMsg.innerHTML="<span>assign value can't be empty or smaller than 5</span>";
        }
-       else{
-        return asignErrMsg.innerHTML="";
-       }
-};
-addTaskBtn.addEventListener('click',validateAsign);
-
-//_____________ ____________ _____________ _________
-
-const dateId = document.getElementById("date_form");
-const dateErrMsg = document.getElementById("date_valid");
-let current = new Date();
-
-
-
-const validateDate=()=>{
-    // let ms1 = dateId.getTime;
-    // let ms2 = current.getTime;
-    // console.log(ms1);
-    // console.log(ms2);
-    if(dateId.value === "" ){
+       else if(dateId.value === "" ){
         dateErrMsg.style.color='red';
        return dateErrMsg.innerHTML="<span>Invalid date</span>";
        }
-       else{
-        return dateErrMsg.innerHTML="";
-       }
-};
-addTaskBtn.addEventListener('click',validateDate);
-
-//_____________ ____________ _____________ _________
-
-const statusId = document.getElementById("inputStatus");
-const statusErrMsg = document.getElementById("status_valid");
-
-const validateStatus=()=>{
-    if(statusId.value === ""){
+       else if(statusId.value === ""){
         statusErrMsg.style.color='red';
        return statusErrMsg.innerHTML="<span>Status can't be empty</span>";
        }
+       
        else{
-        return statusErrMsg.innerHTML="";
+           return  form.addEventListener('submit',dataCatching);
        }
-};
-addTaskBtn.addEventListener('click',validateStatus);
+
+}
+addTaskBtn.addEventListener('click',validateDesc);
+
+const dataCatching=()=>{
+    errMsg.innerHTML="";
+    descErrMsg.innerHTML="";
+    asignErrMsg.innerHTML="";
+    dateErrMsg.innerHTML="";
+    statusErrMsg.innerHTML="";
+   
+
+    taskManager.addTask(
+        nameId.value,
+        descId.value,
+        asignId.value,
+        dateId.value,
+        statusId.value
+      );
+     console.log("Task Name :" +  nameId.value.length);
+     console.log("User Name:"+nameId.value);
+        console.log("Description:"+descId.value);
+        console.log("Assign To:"+asignId.value);
+        console.log("Date:"+dateId.value);
+        console.log("Status:"+statusId.value);
+
+        nameId.value="";
+        descId.value="";
+        asignId.value="";
+        dateId.value="";
+        statusId.value="";
+      
+
+       
+}
---
+++ js/taskmanager.js
@@ -0,0 +1,28 @@
+
+
+ class TaskManager{
+    constructor(currentId=0)
+    {
+        this.tasks=[];
+        this.currentId=currentId;
+     }
+     
+     addTask(name,discription,assignTo,dueDate,status){
+        
+        const task ={
+            id:this.currentId++,
+            name:name,
+            discription:discription,
+            assignTo:assignTo,
+            dueDate:dueDate,
+            status:status
+        };
+        this.tasks.push({task});
+    }
+}
+
+  //const taskManager = new TaskManager(0);
+ //console.log(taskManager.tasks);
+
+
+
